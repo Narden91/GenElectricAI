432bc95c8a12089df657584e48727d7776832c22
Emanuele <eminik2006@gmail.com>
2025-05-14 10:33:29 +0200

    first commit

---
+++ classifier/__init__.py
@@ -0,0 +1,2 @@
+from .classifier import train_and_evaluate_xgboost, train_and_evaluate_catboost, train_and_evaluate_random_forest
+
---
+++ genetic_algorithm/genetic_algorithm.py
@@ -0,0 +1,11 @@
+class EsempioAlgoritmoGenetico:
+    def __init__(self, parametri):
+        self.parametri = parametri
+        print(f"Algoritmo Genetico inizializzato con i parametri: {self.parametri}")
+
+    def ottimizza(self, data):
+        print(f"Ottimizzazione avviata con i dati: {data}")
+        risultato_ottimizzato = "risultato_placeholder"
+        print("Ottimizzazione completata.")
+        return risultato_ottimizzato
+
